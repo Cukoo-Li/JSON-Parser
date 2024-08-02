690270bafcc55aca9249a95873231d7157d1c6bc
Cukoo <liwwcukoo@gmail.com>
2024-08-02 18:02:57 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "json.h"
+
+int main() {
+    std::ifstream ifs("./xxx.json");
+    std::stringstream ss;
+    ss << ifs.rdbuf();
+    std::string s{ss.str()};
+    json::Node x = json::parse(s).value();
+    std::cout << x << '\n';
+    if (x.type() == "Object") {
+        json::Object& root = x.as<json::Object>();
+        root["Cukoo"] = {json::Array{}};
+        root["Cukoo"].as<json::Array>().push_back({json::Value{24}});
+        root["Cukoo"].as<json::Array>().push_back({json::Value{"boy"}});
+        std::cout << x << '\n';
+    }
+}
